3a837cefbc353fdc0418855466fa445e71dd2a4b
Conner Lindsley <connerlindsley@gmail.com>
2018-07-01 21:15:54 -0700

    Simple Mouse Follow Movement
    Made a simple follower script to debug input movement and also make the player follow the mouse. (Only works with one input)

---
+++ GigaScapes/Assets/GameModules/Pong-teroids/TestAssets/Debug/MouseFollower.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Small script used to track the mouse movement over a few frames.
+
+public class MouseFollower : MonoBehaviour
+{
+    private void Awake()
+    {
+        //Hard setting the framerate to 60, as my machine runs WAY faster than that...
+        Application.targetFrameRate = 60;
+    }
+
+    public GameObject tracker;
+    private Queue<GameObject> trackingQueue;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnTracker", 0.0f, 0.05f);
+        trackingQueue = new Queue<GameObject>(0);
+    }
+
+    //Function that is invoked 1/20 frames. Handles all mouse tracker spawning and despawning. Warning: Inefficient!!! (Need to build a little object pooler for this)
+    public void SpawnTracker()
+    {
+        Vector2 v2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 v3 = new Vector3(v2.x, v2.y, 0);
+
+        gameObject.transform.position = v3;
+
+        GameObject temp = Instantiate(tracker, v3, Quaternion.identity);
+
+        //Debug.Log("Spawning Tracker " + temp.name + " at position " + v3.x + " " + v3.y);
+
+        trackingQueue.Enqueue(temp);
+
+        if(trackingQueue.Count > 3)
+        {
+            Destroy(trackingQueue.Dequeue());
+        }
+    }
+}
---
+++ GigaScapes/Assets/PlayerMover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    Vector2 thisVec;
+    Vector2 mouseVec;
+    Rigidbody2D shipRB;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        shipRB = gameObject.GetComponentInChildren<Rigidbody2D>();
+
+    }
+
+    //We'll use this to update the Player's target when pulled from the ObjectPool.
+    private void OnEnable()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        thisVec = gameObject.transform.position;
+        mouseVec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 vel = shipRB.velocity;
+
+        gameObject.transform.position = Vector2.SmoothDamp(thisVec, mouseVec, ref vel, 10.0f, 1.0f, 15.0f);
+
+    }
+}
