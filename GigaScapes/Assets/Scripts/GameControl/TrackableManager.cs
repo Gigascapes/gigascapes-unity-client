3ef6ec1a2d0b7af7ac446fc508428d150c9e8f68
The Capt <jironomics2@gmail.com>
2018-07-09 16:21:53 -0700

    tiny manager

---
+++ GigaScapes/Assets/Scripts/GameControl/TrackableManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackableManager : MonoBehaviour {
+
+    public List<TrackableObject> TrackedObjects = new List<TrackableObject>();
+
+    Dictionary<int[], Detector> killList = new Dictionary<int[], Detector>();
+
+    private void Update()
+    {
+        for(int i = 0; i < TrackedObjects.Count; i++)
+        {
+            TrackedObjects[i].Managed = true;
+
+            killList = TrackedObjects[i].ManagedUpdate(killList);
+        }
+    }
+}
